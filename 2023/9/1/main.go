af86a27649b68e4365ae7ff5651246a171beb819
Dmitry Silchenko <dmitry@desofto.com>
2023-12-09 14:06:36 +0100

    2023-9

---
+++ 2023/9/1/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+  data, err := os.ReadFile("./real.txt")
+  if err != nil { panic(err) }
+  input := strings.Split(string(data), "\n")
+
+  getLine := func () (string, bool) {
+    var line string
+
+    if len(input) <= 0 {
+      return "", true
+    }
+
+    line = strings.Trim(input[0], " ")
+    input = input[1:]
+
+    end := len(input) <= 0 || len(strings.Trim(input[0], " ")) <= 0
+
+    if end {
+      input = input[1:]
+    }
+
+    return line, end
+  }
+
+  sum := 0
+
+  for {
+    line, _ := getLine()
+    if len(line) <= 0 { break }
+
+    var history []int
+    for _, value := range strings.Split(line, " ") {
+      val, err := strconv.Atoi(strings.Trim(value, " "))
+      if err != nil { panic(err) }
+
+      history = append(history, val)
+    }
+
+    nextValue := predict(history)
+    sum += nextValue
+    fmt.Println(sum, history, nextValue)
+  }
+
+  fmt.Println(sum)
+}
+
+func predict(history []int) int {
+  allZeroes := true
+  for _, value := range history {
+    if value != 0 {
+      allZeroes = false
+      break
+    }
+  }
+  if allZeroes { return 0 }
+
+  subsequence := make([]int, len(history)-1)
+  for i := 0; i < len(history)-1; i++ {
+    subsequence[i] = history[i+1] - history[i]
+  }
+
+  return history[len(history)-1] + predict(subsequence)
+}
